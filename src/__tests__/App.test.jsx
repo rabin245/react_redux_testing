8002e0fba254341a849b2bdd6b169965efea9ed9
rabin245 <drarabin@gmail.com>
2023-06-29 12:00:25 +0545

    test the logos in the App component

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 describe("App component", () => {
   it("should render the App component", () => {
@@ -8,5 +8,17 @@ describe("App component", () => {
       name: /vite \+ react/i,
     });
     expect(headingElement).toBeInTheDocument();
+  });
+
+  it("should render the Vite logo", () => {
+    render(<App />);
+    const viteLogo = screen.getByAltText(/vite logo/i);
+    expect(viteLogo).toBeInTheDocument();
+  });
+
+  it("should render the React logo", () => {
+    render(<App />);
+    const viteLogo = screen.getByAltText(/react logo/i);
+    expect(viteLogo).toBeInTheDocument();
   });
 });
